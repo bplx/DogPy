e3b241e881ada17584bb5b6e6e389eee2493b446
bplx <satosummering@protonmail.com>
2021-08-26 16:33:16 +0000

    init commit (rhyme pog)

---
+++ bin/main.py
@@ -0,0 +1,62 @@
+# get dogs
+
+
+# import stuff and setup stuff lol
+import requests
+import json
+import os
+import time
+
+# make variables
+dogcount = 1
+
+def getdog():
+    global dogcount
+    # dog url
+    dog = 'https://dog.ceo/api/breeds/image/random'
+
+    # get request dog
+
+    getdog = requests.get(dog) 
+
+
+    # there was a heap of code here but i removed it
+
+
+    # print dog
+    dogjson = getdog.json()
+    
+    os.system('clear')
+    print('currently viewing: ' + dogjson['message'])
+
+    os.system('feh ' + dogjson['message'])
+    return dogcount + 1
+
+def mainloop():
+    
+    if dogcount == 0:
+        print('welcome to dog program. close feh window for new dog prompt')
+        getdog()
+    
+    if dogcount >= 0:
+        os.system('clear')
+        print('want another dog? [enter] for yes, [q] to quit. (' + str(dogcount) + ' dog(s) viewed)')
+        answer = input()
+        if answer == "":
+            print()
+        elif answer == "q":
+            exit()
+        else: 
+            print(" .                      ")
+            print("/!\ input not recognized")
+            print("'''                     ")
+            time.sleep()
+            mainloop()
+
+# start program
+while True:
+    mainloop()
+    dogcount = getdog()
+
+
+
